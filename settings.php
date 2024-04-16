2a1fa61077f4a0f2d6db09b0556e7e2428136f19
Mihalkevitc <144014512+Mihalkevitc@users.noreply.github.com>
2024-04-16 20:09:13 +0300

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,9 @@
+$databases['default']['default'] = array(
+  'database' => 'your_database_name',
+  'username' => 'your_username',
+  'password' => 'your_password',
+  'host' => 'mysql',
+  'port' => '3306',
+  'driver' => 'mysql',
+  'prefix' => '',
+);
